679d2d694c4793324f650b78bcbeefb6cfaca71d
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-07-25 13:57:40 +0700

    feat: set up root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main className="flex min-h-screen select-none flex-col items-center justify-center gap-y-4 p-24">
+      <h1 className="text-xl sm:text-2xl lg:text-4xl">Cake List App</h1>
+      <Link
+        href={"/cakes"}
+        className="rounded-md bg-sky-500 px-4 py-2 text-white"
+      >
+        Get Started
+      </Link>
     </main>
   );
 }
