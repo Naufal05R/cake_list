eb7810643bacbbdc3d23b9ec00a50a2f6d39a130
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-07-25 18:03:37 +0700

    feat: fetching data to cake-table.tsx Component

--- components/cake-table.tsx
+++ components/cake-table.tsx
@@ -1,13 +1,16 @@
+import { getCakes } from "@/lib/data";
 import React from "react";
 
-const CakeTable = () => {
+const CakeTable = async () => {
+  const cakes = await getCakes();
+
   return (
     <table className="w-full text-left text-sm text-gray-500">
       <thead className="bg-gray-50 text-sm uppercase text-gray-700">
         <tr>
           <th className="w-14 min-w-14 max-w-14 px-6 py-3">#</th>
           <th className="w-full min-w-96 max-w-full px-6 py-3">Name</th>
-          <th className="w-52 min-w-52 max-w-52 px-6 py-3">Category</th>
+          <th className="w-52 min-w-52 max-w-52 px-6 py-3 text-center">Category</th>
           <th className="w-28 min-w-28 max-w-28 px-6 py-3 text-center">
             Price
           </th>
@@ -17,13 +20,17 @@ const CakeTable = () => {
         </tr>
       </thead>
       <tbody>
-        <tr className="border-b bg-white">
-          <td className="w-14 min-w-14 max-w-14 px-6 py-3"></td>
-          <td className="w-full min-w-96 max-w-full px-6 py-3"></td>
-          <td className="w-52 min-w-52 max-w-52 px-6 py-3"></td>
-          <td className="w-32 min-w-32 max-w-32 px-6 py-3 text-center"></td>
-          <td className="flex w-32 min-w-32 max-w-32 items-center justify-center gap-x-1.5 px-6 py-3"></td>
-        </tr>
+        {cakes.map((cake, index) => (
+          <tr key={cake.id} className="border-b bg-white">
+            <td className="w-14 min-w-14 max-w-14 px-6 py-3">{index + 1}</td>
+            <td className="w-full min-w-96 max-w-full px-6 py-3">{cake.name}</td>
+            <td className="w-52 min-w-52 max-w-52 px-6 py-3 text-center">{cake.category}</td>
+            <td className="w-32 min-w-32 max-w-32 px-6 py-3 text-center">{cake.price}</td>
+            <td className="flex w-32 min-w-32 max-w-32 items-center justify-center gap-x-1.5 px-6 py-3">
+
+            </td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
