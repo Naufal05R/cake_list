883ededab74c67ec08632dbbad759e744021481e
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-07-27 06:01:24 +0700

    feat: add formatPrice utility function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,15 @@
-import clsx, { ClassValue} from "clsx";
+import clsx, { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export const cn = (...inputs: ClassValue[]) => {
-    return twMerge(clsx(inputs));
-}
+  return twMerge(clsx(inputs));
+};
+
+export const formatPrice = (priceParam: string | number) => {
+  const price = Number(priceParam);
+  const formatter = new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+  });
+  return formatter.format(price);
+};
