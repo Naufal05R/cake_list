85ca6e3fb8a8e51a65fffd4a5a1127dc1a0b4f02
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-07-26 21:43:46 +0700

    feat: add CreateButton

---
+++ components/button.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import { IoAddSharp } from "react-icons/io5";
+
+export const CreateButton = () => {
+  return (
+    <Link
+      href="/animals/create"
+      className="inline-flex cursor-pointer items-center gap-x-1 rounded-sm bg-blue-700 py-[9px] pl-2.5 pr-4 text-sm text-white hover:bg-blue-800"
+    >
+      <IoAddSharp size={20} />
+    </Link>
+  );
+};
