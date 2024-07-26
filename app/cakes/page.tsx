1b3955adb138d6e3d5a7da8c80cae62fae429ed0
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-07-26 21:44:31 +0700

    feat: add CreateButton to UI

--- app/cakes/page.tsx
+++ app/cakes/page.tsx
@@ -1,3 +1,4 @@
+import { CreateButton } from "@/components/button";
 import CakeTable from "@/components/cake-table";
 import Search from "@/components/search";
 
@@ -6,6 +7,7 @@ export default function CakesPage({}) {
     <main className="relative mx-auto min-h-screen max-w-screen-xl px-4 py-2 sm:px-8 sm:py-4 lg:px-16 lg:py-8">
       <hgroup className="mb-5 flex items-center justify-between gap-1">
         <Search />
+        <CreateButton />
       </hgroup>
       <section className="max-w-full overflow-x-scroll">
         <CakeTable />
