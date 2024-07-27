43f54c569e420e524788bdfc71a55b15e9537efd
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-07-27 19:02:10 +0700

    fix: remove additional ,00 from price

--- lib/utils.ts
+++ lib/utils.ts
@@ -10,6 +10,8 @@ export const formatPrice = (priceParam: string | number) => {
   const formatter = new Intl.NumberFormat("id-ID", {
     style: "currency",
     currency: "IDR",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
   });
   return formatter.format(price);
 };
