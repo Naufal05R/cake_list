f059d8b84b0994e29dd98b0dede57af0d3125fa7
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-07-28 22:35:13 +0700

    feat: fix FilterButton UI

--- components/buttons.tsx
+++ components/buttons.tsx
@@ -3,9 +3,7 @@ import { VscSettings } from "react-icons/vsc";
 
 export const CreateButton = () => {
   return (
-    <button
-      className="inline-flex cursor-pointer items-center gap-x-1 rounded-sm bg-blue-700 py-[9px] pl-2.5 pr-4 text-sm text-white hover:bg-blue-800"
-    >
+    <button className="inline-flex cursor-pointer items-center gap-x-1 rounded-sm bg-blue-700 py-[9px] pl-2.5 pr-4 text-sm text-white hover:bg-blue-800">
       <IoAddSharp size={20} />
       Create
     </button>
@@ -14,17 +12,15 @@ export const CreateButton = () => {
 
 export const FilterButton = () => {
   return (
-    <button className="inline-flex cursor-pointer rounded-sm border p-1 hover:bg-gray-100">
-      <VscSettings />
+    <button className="inline-flex cursor-pointer items-center rounded-sm border p-2 text-sm hover:bg-gray-100">
+      <VscSettings size={20} />
     </button>
   );
-}
+};
 
 export const EditButton = () => {
   return (
-    <button
-      className="inline-flex cursor-pointer rounded-sm border p-1 hover:bg-gray-100"
-    >
+    <button className="inline-flex cursor-pointer rounded-sm border p-1 hover:bg-gray-100">
       <IoPencil size={20} />
     </button>
   );
