a973b62f41c28393af200129289afe1ef41e1113
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-07-29 20:31:46 +0700

    feat: create SortButton

--- components/buttons.tsx
+++ components/buttons.tsx
@@ -4,6 +4,8 @@ import { IoAddSharp, IoPencil, IoTrashOutline } from "react-icons/io5";
 import { VscSettings } from "react-icons/vsc";
 import { useState } from "react";
 import { FilterModal } from "./modals";
+import { IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io";
+import { cn } from "@/lib/utils";
 
 export const CreateButton = () => {
   return (
@@ -25,6 +27,21 @@ export const FilterButton = () => {
         <VscSettings size={20} />
       </button>
       <FilterModal isOpen={isOpen} />
+    </>
+  );
+};
+
+export const SortButton = () => {
+  const [isTop, setIsTop] = useState(true);
+  return (
+    <>
+      <button
+        onClick={() => setIsTop(!isTop)}
+        className="flex h-[38px] w-[38px] cursor-pointer items-center justify-center rounded-sm border bg-blue-700 text-sm text-white hover:bg-blue-800"
+      >
+        <IoMdArrowDropup className={cn({ hidden: isTop })} size={32} />
+        <IoMdArrowDropdown className={cn({ hidden: !isTop })} size={32} />
+      </button>
     </>
   );
 };
