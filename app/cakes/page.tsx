10aede0f90c81270847bf975b8faae11bddb5f50
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-07-29 20:31:55 +0700

    feat: add SortButton to UI

--- app/cakes/page.tsx
+++ app/cakes/page.tsx
@@ -1,4 +1,4 @@
-import { CreateButton, FilterButton } from "@/components/buttons";
+import { CreateButton, FilterButton, SortButton } from "@/components/buttons";
 import CakeTable from "@/components/cake-table";
 import Search from "@/components/search";
 
@@ -7,6 +7,7 @@ export default function CakesPage({}) {
     <main className="relative mx-auto min-h-screen max-w-screen-xl px-4 py-2 sm:px-8 sm:py-4 lg:px-16 lg:py-8">
       <hgroup className="mb-5 flex items-center justify-between gap-1">
         <FilterButton />
+        <SortButton />
         <Search />
         <CreateButton />
       </hgroup>
